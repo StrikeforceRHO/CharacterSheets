e11192df4f2079f8bf8637361f83fd8c3eba816d
nkremerh <nkremerh@nd.edu>
2017-03-29 20:37:46 -0400

    Modifies error message Adds extra git add for new files

--- commit.pl
+++ commit.pl
@@ -8,14 +8,14 @@ if($num_args eq 1) {
 	$pdf = $ARGV[0];
 }
 else {
-	print(STDERR "Arguments needed: full filename of PDF file.\n");
+	print(STDERR "Arguments needed: full filename of changed file.\n");
 	exit 1;
 }
 
 open(INPUT, $pdf) or die("Could not find $pdf. Please make sure $pdf is in the current directory.");
 close(INPUT);
 
-my $result = system("git pull --rebase origin master ; git add -u ; git commit -m \"Modifies $pdf after last session\" ; git push origin master");
+my $result = system("git pull --rebase origin master ; git add -u ; git add $pdf ; git commit -m \"Modifies $pdf after last session\" ; git push origin master");
 if(!$result) {
 	print("Changes to $pdf committed successfully.\n");
 }
